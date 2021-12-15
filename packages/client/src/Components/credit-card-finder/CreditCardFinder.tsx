2f9592c4636e5c80d831586db67a464dc1cbefd0
baileyg2016 <bspell20@vt.edu>
2021-12-14 22:47:40 -0800

    almost have the calculation method completed

--- packages/client/src/Components/credit-card-finder/CreditCardFinder.tsx
+++ packages/client/src/Components/credit-card-finder/CreditCardFinder.tsx
@@ -1,25 +1,28 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
-import Context from "../../context/Context";
 import "./CreditCardFinder.scss";
 import {
     Data,
     DataItem,
     transformTransactionsData,
 } from "../../dataUtilities";
+import cards from '../../creditCards/creditCards.json';
 
 export const CreditCardFinder = () => {
-    const {
-        itemId,
-        accessToken,
-        linkToken,
-        linkSuccess,
-        isItemAccess,
-        backend,
-        linkTokenError,
-    } = useContext(Context);
+    // const {
+    //     itemId,
+    //     accessToken,
+    //     linkToken,
+    //     linkSuccess,
+    //     isItemAccess,
+    //     backend,
+    //     linkTokenError,
+    // } = useContext(Context);
 
     const [transformedData, setTransformedData] = useState<Data>([]);
+    const [categories, setCategories] = useState<Array<string>>([]);
+    const [amounts, setAmounts] = useState<Array<number>>([]);
+    const [totalPoints, setTotalPoints] = useState<Array<{name: string, points: number}>>([]);
 
     const getData = async () => {
         const response = await fetch(`${process.env.API}/api/transactions`, { method: "GET" });
@@ -30,58 +33,74 @@ export const CreditCardFinder = () => {
         setTransformedData(transformTransactionsData(data));
     };
 
-
-    useEffect(() => {
-        getData(); 
-    }, []);
-
-
-    let amounts_by_category : Map<string, number> = new Map();
-    transformedData.forEach((dataItem : DataItem | any) => {
-
-        // Categories Key
-        const category = dataItem["category"][0];
-        // Amount Value
-        console.log(dataItem["amount"])
-        const amountString = dataItem["amount"] ?? 0;
-        
-        let amount : number = +(amountString.replace("USD", ""));
-
-        if (category == null) {
-            return; 
-        }
-        else {
-            // categories.forEach((category : string) => {
-            //     amount = amounts_by_category.has(category) ? amounts_by_category.get(category) as number + amount : amount;
-            //     amounts_by_category.set(category, amount);
-            // });
-            console.log("\n")
-            console.log(dataItem["name"])
-            console.log(dataItem["category"])
-            console.log(dataItem["amount"])
-            const newAmount = amounts_by_category.has(category) ? amounts_by_category.get(category) as number + amount : amount;
-            amounts_by_category.set(category, newAmount)
-        }
-        return amounts_by_category;
-    }); 
-
-
-    const categories: Array<string> = [];       
-    const amounts: Array<number> = [];
+    const getCategories = useCallback(async () => {
+        let amounts_by_category : Map<string, number> = new Map();
+        transformedData.map((dataItem : DataItem | any) => {
     
-    amounts_by_category.forEach((value: number, key: string) => {
+            // Categories Key
+            const category = dataItem["category"][0];
+            // Amount Value
+            console.log(dataItem["amount"])
+            const amountString = dataItem["amount"] ?? 0;
+            
+            let amount : number = +(amountString.replace("USD", ""));
+    
+            if (category == null) {
+                return; 
+            }
+            else {
+                // categories.forEach((category : string) => {
+                //     amount = amounts_by_category.has(category) ? amounts_by_category.get(category) as number + amount : amount;
+                //     amounts_by_category.set(category, amount);
+                // });
+                console.log("\n")
+                console.log(dataItem["name"])
+                console.log(dataItem["category"])
+                console.log(dataItem["amount"])
+                const newAmount = amounts_by_category.has(category) ? amounts_by_category.get(category) as number + amount : amount;
+                amounts_by_category.set(category, newAmount)
+            }
+            return amounts_by_category;
+        }); 
+    
+    
+        const categories: Array<string> = [];       
+        const amounts: Array<number> = [];
+        
+        amounts_by_category.forEach((value: number, key: string) => {
             categories.push(key);
             amounts.push(value);
-    })
+        });
 
-    const rows = categories.map((category: string, index) => {
-        return (
-            <tr key={category} className='dataField'>
-                <td>{category}</td>
-                <td>{amounts[index]}</td>
-            </tr>
-        )
-    })
+        setCategories(categories);
+        setAmounts(amounts);
+    }, [transformedData]);
+
+    const calculateCards = useCallback(() => {
+        const cardPoints = cards.cards.map(({ name, points, fee }, _index) => {
+            const total = 0;
+
+            
+
+            return {
+                name,
+                points: total,
+            }
+        });
+        setTotalPoints(cardPoints);
+    }, []);
+
+    useEffect(() => {
+        const init = async () => {
+            await getData();
+        };
+
+        init();
+    }, []);
+
+    useEffect(() => {
+        getCategories();
+    }, [getCategories]);
 
     return (    
         <>
@@ -89,14 +108,21 @@ export const CreditCardFinder = () => {
             <div className='CreditCardFinder'>
                 <table>
                     <thead>
-                        <tr>Transaction history</tr>
+                        <td>Transaction history</td>
                     </thead>
                     <tbody>
                         <tr>
                             <td>Category</td>
                             <td>Transaction</td>
                         </tr>
-                        {rows}
+                        {
+                            categories.map((category: string, index) => (
+                                <tr key={category} className='dataField'>
+                                    <td>{category}</td>
+                                    <td>{amounts[index]}</td>
+                                </tr>
+                            ))
+                        }
                     </tbody>
                 </table>
             </div>  
